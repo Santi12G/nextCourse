84fac130d8619afc403d295f62847043a5c93b01
Santi12G <santiagogrande87@gmail.com>
2025-12-16 18:47:46 -0500

    NEW: archivos terminados hasta el condicional del navBar

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+import "@/app/dashboard/customers/styles.css"
+
+export default function Page(){
+    return <p className="customer-title">Customers Page</p>;
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,19 @@
+/*
+
+This document would be used to keep fonts that will be used
+in the app
+
+
+            /
+....       /        ----
+      |||||--------!    !
+....       \        ----
+            \
+
+
+*/
+
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin']});
+export const lusitana = Lusitana( { weight: ['400', '700'], subsets: ['latin'],});
